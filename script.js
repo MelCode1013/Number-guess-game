827524c79bd0755276020617fc1f50dcfa1b31d2
Melinna <melinna.art@gmail.com>
2024-01-24 21:52:48 -0500

    finish logic for game and add in manipulation of css styles based on case

--- script.js
+++ script.js
@@ -13,52 +13,62 @@
 
 //Adding in random number generator
 
-const randomNum = Number(Math.trunc(Math.random() * 20 + 1))
-const message = document.querySelector('.message')
+let randomNum = Number(Math.trunc(Math.random() * 20 + 1))
+let message = document.querySelector('.message')
 const scoreDisplay = document.querySelector('.score')
 let score = Number(scoreDisplay.textContent)
 const showCorrectNum = document.querySelector('.number')
 const highScoreDisplay = document.querySelector('.highscore')
 let highScore = Number(highScoreDisplay.textContent)
+let background = document.querySelector('body')
 
 console.log(randomNum)
-console.log(score)
 
 
 const btn = document.querySelector('.check')
 const againBtn = document.querySelector('.again')
 
 btn.addEventListener('click', function () {
-    const guess = Number(document.querySelector('.guess').value)
+    let guess = Number(document.querySelector('.guess').value)
     if (!guess) {
         message.textContent = "Please enter a number to play!"
     } else if (guess === randomNum) {
         message.textContent = 'You won!'
         showCorrectNum.textContent = randomNum
         console.log('Message should change to "you won!"')
+        background.style.backgroundColor = '#49be25'
+
         //add logic for highscore after win
         if (score > highScore) {
             highScore = score
             highScoreDisplay.textContent = highScore
-            console.log(highScore)
         }
-    } else if (guess > randomNum) {
-        if (score > 1) {
-            message.textContent = "Too high!"
-            score--
-            scoreDisplay.textContent = score
-        } else {
-            message.textContent = "You lost the game :(. Play again?"
-            scoreDisplay.textContent = 0
-        }
-    } else if (guess < randomNum) {
-        message.textContent = "Too low!"
+    } else if ((guess < 1) || (guess > 20)) {
+        message.textContent = "Please enter a number between 1 and 20!"
+    } else if (score > 1) {
+        guess > randomNum ? message.textContent = "Too high!" : message.textContent = "Too low!"
         score--
         scoreDisplay.textContent = score
+    } else {
+        message.textContent = "You lost the game :(. Play again?"
+        scoreDisplay.textContent = 0
+        background.style.backgroundColor = '#8d1919'
     }
+    // } else if (guess < randomNum) {
+    //     message.textContent = "Too low!"
+    //     score--
+    //     scoreDisplay.textContent = score
+    // }
 })
 
 againBtn.addEventListener('click', function () {
-    const refreshRandomNum = Number(Math.trunc(Math.random() * 20 + 1))
-    console.log(refreshRandomNum)
+    randomNum = Number(Math.trunc(Math.random() * 20 + 1))
+    console.log(randomNum)
+    scoreDisplay.textContent = 20
+    score = 20
+    showCorrectNum.textContent = '?'
+    document.querySelector('.guess').value = ''
+    message.textContent = 'Start guessing...'
+    background.style.backgroundColor = '#222'
+
 })
